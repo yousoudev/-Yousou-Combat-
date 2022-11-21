29f62fc4d799ef6b454bd8152fdcc0911715d796
yousoudev <dejongjoost03@gmail.com>
2022-11-21 16:42:02 +0100

    Initial commit

---
+++ CombatPlus/src/main/java/org/yousou/CombatPlus/CombatPlus.java
@@ -0,0 +1,17 @@
+package org.yousou.CombatPlus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CombatPlus extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
